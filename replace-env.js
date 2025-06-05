5ea79a856b9dfb53f03f3f9ac84868816f0e1c49
pgtrunghd <pgtrung1997@gmail.com>
2025-06-05 11:26:23 +0700

    update env.js

---
+++ replace-env.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = './dist/online-management-angular/assets/env.js';
+
+const apiUrl = process.env.API_URL;
+
+let content = fs.readFileSync(path, 'utf8');
+content = content.replace('__API_URL__', apiUrl);
+fs.writeFileSync(path, content);
+
+console.log(`✅ API_URL injected into env.js: ${apiUrl}`);
---
+++ src/assets/env.js
@@ -0,0 +1,4 @@
+(function (window) {
+  window["env"] = window["env"] || {};
+  window["env"]["API_URL"] = "__API_URL__";
+})(this);
